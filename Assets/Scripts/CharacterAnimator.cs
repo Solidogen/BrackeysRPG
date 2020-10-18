4fc10cf235939a756e555eb982a4d8009c05a863
solidogen <solidogen@gmail.com>
2020-10-18 22:55:12 +0200

    Player move animation

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private const float locomotionAnimationSmoothTime = 0.1f;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercent = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
+        animator.SetFloat("SpeedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
