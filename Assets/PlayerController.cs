593fd302960396537d90d2f0be2e6b431d31411b
solidogen <solidogen@gmail.com>
2020-10-18 17:07:59 +0200

    Nav mesh moving to click position

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask movementMask;
+
+    private Camera cam;
+    private PlayerMotor motor;
+    private float raycastMaxDistance = 100;
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // LMB
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, raycastMaxDistance, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+                // Stop focusing any objects
+            }
+        }
+    }
+}
