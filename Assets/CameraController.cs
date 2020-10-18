a7b3b37127035ef562dabf58b958d50a333392c2
solidogen <solidogen@gmail.com>
2020-10-18 17:53:42 +0200

    Rotate camera by horizontal axis

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -14,11 +14,33 @@ public class CameraController : MonoBehaviour
     private float pitch = 2f;
 
     [SerializeField]
+    private float minZoom = 5f;
+
+    [SerializeField]
+    private float maxZoom = 15f;
+
+    [SerializeField]
+    private float zoomSpeed = 4f;
+
+    [SerializeField]
+    private float yawSpeed = 100f;
+
     private float currentZoom = 10f;
+    private float currentYaw = 0f;
+
+    void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
 
     void LateUpdate()
     {
         transform.position = target.position - offset * currentZoom;
         transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
     }
 }
